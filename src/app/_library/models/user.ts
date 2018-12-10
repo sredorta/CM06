d500b4d296fae20634e784e6bd3029cf95ba4faa
sredorta <sergi.redorta@hotmail.com>
2018-12-10 13:24:36 +0100

    Migration of lib

---
+++ src/app/_library/models/user.ts
@@ -0,0 +1,102 @@
+
+import {IApiUserAuth} from '../services/api.service';
+
+export enum avatarSizes  {
+    full = 0, large = 1, big = 2, medium = 3, small = 4, thumbnail = 5, tinythumbnail = 6
+  }
+
+export class User {
+    id : number;
+    firstName:string;
+    lastName:string;
+    email:string;
+    mobile:string;
+    language:string;
+    updated_at:string;
+    created_at:string;
+    account:string;   
+    notifications:number;
+    messages:number;
+    roles:Array<String>;
+    groups:Array<String>;
+    avatar:any;
+    constructor(user: IApiUserAuth) {
+        if (user !== null) {
+            this.id = user.id;
+            this.firstName = user.firstName;
+            this.lastName = user.lastName;
+            this.email = user.email;
+            this.mobile = user.mobile;
+            this.updated_at = user.updated_at;
+            this.created_at = user.created_at;
+            this.account = user.account;
+            this.notifications = user.notifications;
+            this.messages = user.messages;
+            this.roles = user.roles;
+            this.groups = user.groups;
+            this.avatar = user.avatar;
+        } else {
+            this.id = null;
+        }
+        console.log("Resulting user: ");
+        console.log(this);
+    }
+
+    //Returns if we have an user (i.e. we are logged in for current user!)
+    isAvailable() :boolean {
+        if ((this.id !== null) && (this.id !== undefined)) return true;
+        return false;
+    }
+
+    getAvatar(size:avatarSizes) : string {
+        let avatar;
+        switch (size) {
+            case avatarSizes.full:
+                avatar = this.avatar.full==null?null:this.avatar.full.url;
+                break;
+            case avatarSizes.big:
+                avatar =  this.avatar.big==null?null:this.avatar.big.url;
+                break;
+            case avatarSizes.large:
+                avatar = this.avatar.large==null?null:this.avatar.large.url;
+                break;
+            case avatarSizes.medium:
+                avatar = this.avatar.medium==null?null:this.avatar.medium.url;
+                break;
+            case avatarSizes.small:
+                avatar =  this.avatar.small==null?null:this.avatar.small.url;
+                break;            
+            case avatarSizes.thumbnail:
+                avatar =  this.avatar.thumbnail==null?null:this.avatar.thumbnail.url;
+                break;  
+            case avatarSizes.tinythumbnail:
+                avatar = this.avatar.tinythumbnail==null?null:this.avatar.tinythumbnail.url;
+                break; 
+            default:
+                avatar =  this.avatar.medium.url;
+        }
+        if (avatar == null) avatar = this.avatar.full.url; //Case of default
+        return "url(" + avatar + ")";
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    // Token related
+    /////////////////////////////////////////////////////////////////////////
+    public static removeToken() {
+        localStorage.removeItem('jwt-token')
+    }
+
+    public static saveToken(token:string) {
+        localStorage.setItem('jwt-token',token);
+    }
+    public static getToken() : string {
+        return localStorage.getItem('jwt-token');
+    }
+
+    public static hasValidToken() : boolean {
+        //Here we need to validate the token
+        if (localStorage.getItem('jwt-token')== null) 
+           return false;
+        return true;
+    }    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,78 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from '@angular/forms';
+import {FormBuilder, FormGroup, FormControl,ReactiveFormsModule, Validators} from '@angular/forms';
+//MATERIAL DESIGN
+//Material design
+import {MatIconRegistry} from '@angular/material';
+import {MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule} from '@angular/material';
 
+//PRIME-NG COMPONENTS
+import {PasswordModule} from 'primeng/password';
+import {InputTextModule} from 'primeng/inputtext';
+import {ListboxModule} from 'primeng/listbox';
+import {ColorPickerModule} from 'primeng/colorpicker';
+import {CheckboxModule} from 'primeng/checkbox';
+import {ButtonModule} from 'primeng/button';
+import {GMapModule} from 'primeng/gmap';
+import {TooltipModule} from 'primeng/tooltip';
+
+// NGX-TRANSLATE 
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+/////////////////// LIBRARY //////////////////////////////////////////////////
+//HTTP
+import {HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {HttpHeaderInterceptor} from './_library/helpers/http-header-interceptor';
+import {ErrorInterceptor} from './_library/helpers/error.interceptor';
+import {CustomValidators} from './_library/helpers/custom.validators';
+//POPUPS
+import { ErrorSheetComponent } from './_library/helpers/error-sheet/error-sheet.component';
+import { TermsDialogComponent } from './_library/auth/terms-dialog/terms-dialog.component';
+//PIPES
+import { NiceDateFormatPipe } from './_library/pipes/nice-date-format.pipe';
+//SERVICES
+import {ApiService} from './_library/services/api.service';
+/////////////////// END LIBRARY //////////////////////////////////////////////
+
+//COMPONENTS
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +82,77 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NiceDateFormatPipe,
+    [PasswordModule, //PRIME-NG modules
+    InputTextModule,
+    ListboxModule,
+    CheckboxModule,
+    ButtonModule,
+    ColorPickerModule,
+    GMapModule,
+    TooltipModule
+    ],
+    [  MatAutocompleteModule, //MATERIAL2
+      MatBadgeModule,
+      MatBottomSheetModule,
+      MatButtonModule,
+      MatButtonToggleModule,
+      MatCardModule,
+      MatCheckboxModule,
+      MatChipsModule,
+      MatDatepickerModule,
+      MatDialogModule,
+      MatDividerModule,
+      MatExpansionModule,
+      MatGridListModule,
+      MatIconModule,
+      MatInputModule,
+      MatListModule,
+      MatMenuModule,
+      MatNativeDateModule,
+      MatPaginatorModule,
+      MatProgressBarModule,
+      MatProgressSpinnerModule,
+      MatRadioModule,
+      MatRippleModule,
+      MatSelectModule,
+      MatSidenavModule,
+      MatSliderModule,
+      MatSlideToggleModule,
+      MatSnackBarModule,
+      MatSortModule,
+      MatStepperModule,
+      MatTableModule,
+      MatTabsModule,
+      MatToolbarModule,
+      MatTooltipModule,
+      MatTreeModule         
+    ],    
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    }),
   ],
-  providers: [],
+  entryComponents: [ErrorSheetComponent,TermsDialogComponent],
+  providers: [
+    HttpClient, ApiService,
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }, 
+    {provide: HTTP_INTERCEPTORS, useClass: HttpHeaderInterceptor, multi: true }    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(https: HttpClient) : TranslateHttpLoader {
+  return new TranslateHttpLoader(https, './assets/i18n/', '.json');
+}
