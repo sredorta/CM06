e051fc9e1014f4ebb8471ad679098c752e8e1bf8
sredorta <sergi.redorta@hotmail.com>
2019-01-17 09:46:19 +0100

    Updated interceptor

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -6,14 +6,18 @@ import { Observable, throwError } from 'rxjs';
 import {User} from '../_models/user';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import {SidebarModule} from 'primeng/sidebar';
+
 import 'rxjs/add/operator/do';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 //We intercept all http requests and parse error and return correct error message
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private bottomSheet: MatBottomSheet, private router : Router) {}
+    private _subscription : Subscription = new Subscription();
+
+    constructor(private bottomSheet: MatBottomSheet, private router : Router,private translate: TranslateService) {}
     
     //Opens bottomsheet with error or success message
     openBottomSheet(type:string, code:number,  message:string): void {
@@ -26,8 +30,9 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
 
     getText(msg:string) {
-        if (msg.length > 500) 
-            msg = msg.substr(0, 500) + '...';
+        if (msg)
+            if (msg.length > 500) 
+                msg = msg.substr(0, 500) + '...';
         return msg;
     }
 
@@ -43,31 +48,40 @@ export class ErrorInterceptor implements HttpInterceptor {
                 // any way to alter response that gets sent to the request subscriber?
             }
         }, (error: any) => {    
-            if (error instanceof HttpErrorResponse) {
-                if (error.error.exception === 'Tymon\\JWTAuth\\Exceptions\\TokenExpiredException' || 
-                   error.error.exception === 'Tymon\\JWTAuth\\Exceptions\\TokenBlacklistedException') {
-                    //this.userService.logout();
-                    User.removeToken();
-                    //this.userService.setCurrent(new User(null));  
-                    if (error.error.message != null) 
-                        this.openBottomSheet("error",error.status,this.getText(error.error.message));
-                    else
-                        this.openBottomSheet("error",error.status,"Votre session n'est pas pas valide ou votre acces n'est pas autorize");                     
-                    this.router.navigate(['/login']);  
+            this._subscription = this.translate.get(["interceptor.session.invalid","interceptor.unknown"]).subscribe( trans => {
+                if (error instanceof HttpErrorResponse) {
+                    console.log("Error interceptor");
+                    console.log(error);
+                    if (error.error.exception === 'Tymon\\JWTAuth\\Exceptions\\TokenExpiredException' || 
+                    error.error.exception === 'Tymon\\JWTAuth\\Exceptions\\TokenBlacklistedException') {
+                        //this.userService.logout();
+                        User.removeToken();
+                        //this.userService.setCurrent(new User(null));  
+                        if (error.error.message != null) 
+                            this.openBottomSheet("error",error.status,this.getText(error.error.message));
+                        else
+                            this.openBottomSheet("error",error.status,trans["interceptor.session.invalid"]);                     
+                        this.router.navigate(['/login']);  
+                    } 
+                    if (error.status === 401 || error.status === 403) {
+                        //this.userService.logout();
+                        //User.removeToken();
+                        //this.userService.setCurrent(new User(null));  
+                        if (error.error.message != null) 
+                            this.openBottomSheet("error",error.status,this.getText(error.error.message));
+                        else
+                            this.openBottomSheet("error",error.status,trans["interceptor.session.access"]);                     
+                        //this.router.navigate(['/login']);  
+                        } else {
+                            this.openBottomSheet(error.error.response, error.status, this.getText(error.error.message) || error.statusText);
+                    } 
+                    if(error.status === 0) {
+                        //Network disconnected
+                        this.openBottomSheet("error",404,trans["interceptor.unknown"]);
+                    }
                 }
-                if (error.status === 401 || error.status === 403) {
-                    //this.userService.logout();
-                    //User.removeToken();
-                    //this.userService.setCurrent(new User(null));  
-                    if (error.error.message != null) 
-                        this.openBottomSheet("error",error.status,this.getText(error.error.message));
-                    else
-                        this.openBottomSheet("error",error.status,"Votre session n'est pas pas valide ou votre acces n'est pas autorize");                     
-                    //this.router.navigate(['/login']);  
-                    } else {
-                        this.openBottomSheet(error.error.response, error.status, this.getText(error.error.message) || error.statusText);
-                }
-            }
+                this._subscription.unsubscribe();
+            });
         });
     }
 }
