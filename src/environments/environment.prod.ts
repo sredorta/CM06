788a0333e64ce99d2511ecacb686f3e7d92eab04
sredorta <sergi.redorta@hotmail.com>
2019-01-18 09:39:01 +0100

    Completed cart

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiURL: 'http://www.ecube-solutions.com/Api/public/api',
   gmapKey: 'AIzaSyDJX3_xSyhfZCyA2Z20f1d74X8sHOkX9dE',
-  fbKey : '397476587654542'
+  fbKey : '397476587654542' 
 };
