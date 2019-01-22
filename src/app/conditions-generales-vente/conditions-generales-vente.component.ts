60e95478a28218f7df313958b79ef665ea2d7296
sredorta <sergi.redorta@hotmail.com>
2019-01-22 09:44:06 +0100

    Before changing product

--- src/app/conditions-generales-vente/conditions-generales-vente.component.ts
+++ src/app/conditions-generales-vente/conditions-generales-vente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
-
+import { DataService } from '../_services/data.service';
+import {Config, EApiConfigKeys} from '../_models/config';
 @Component({
   selector: 'app-conditions-generales-vente',
   templateUrl: './conditions-generales-vente.component.html',
@@ -8,9 +9,12 @@ import { Location } from '@angular/common';
 })
 export class ConditionsGeneralesVenteComponent implements OnInit {
  @Input() onlyText : boolean = false;
-  constructor(private _location: Location) { }
+ config : Config = new Config(this.data.getConfig());
+ address : string = this.config.get(EApiConfigKeys.address);
+  constructor(private _location: Location, private data: DataService) { }
 
   ngOnInit() {
+    
   }
 
   //Go back 
--- src/app/mentions-legales/mentions-legales.component.ts
+++ src/app/mentions-legales/mentions-legales.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { DataService } from '../_services/data.service';
+import {Config, EApiConfigKeys} from '../_models/config';
 @Component({
   selector: 'app-mentions-legales',
   templateUrl: './mentions-legales.component.html',
   styleUrls: ['./mentions-legales.component.scss']
 })
 export class MentionsLegalesComponent implements OnInit {
-
-  constructor(private _location: Location) { }
+  config : Config = new Config(this.data.getConfig());
+  address : string = this.config.get(EApiConfigKeys.address);
+  constructor(private _location: Location, private data: DataService) { }
 
   ngOnInit() {
   }
